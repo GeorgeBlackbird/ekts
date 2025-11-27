0197513f4af0b48158336a18e246d5b7a6513d67
GeorgeBlackbird <drozdov.jorj@yandex.ru>
2025-11-27 10:13:12 +0500

    Config

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  imports: {
+    autoImport: true,
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
